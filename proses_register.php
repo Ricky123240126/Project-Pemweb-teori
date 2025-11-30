d06c80e301575ac44d8c967db406bbf0a4d9104e
Ricky <Rickymhmdhrnsyh@gmail.com>
2025-11-30 13:24:36 +0700

    gaatau

--- proses_register.php
+++ proses_register.php
@@ -4,7 +4,7 @@ include 'connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = trim($_POST['nama']);
     $username = trim($_POST["username"]);
-    $password = trim($_POST["passwor"]);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $role = 'admin'; 
 
     $cek_sql = "SELECT * FROM admin WHERE username = ?";
@@ -16,6 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($cek_result->num_rows > 0) {
         echo "<script>alert('Username telah digunakan! Gunakan username lain'); window.location='register.php';</script>";
     } else {
+        // Insert dengan prepared statement
         $insert_sql = "INSERT INTO admin (nama, username, password, role) VALUES (?, ?, ?, ?)";
         $insert_stmt = $connection->prepare($insert_sql);
         $insert_stmt->bind_param("ssss", $nama, $username, $password, $role);
