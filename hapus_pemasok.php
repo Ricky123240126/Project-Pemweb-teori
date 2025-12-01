dbcbaea4abc6fd22f1ccad06e9d027aac0db2c22
Ricky123240126 <rickymhmdhrnsyh@gmail.com>
2025-12-01 22:15:24 +0700

    Delete hapus_pemasok.php

--- hapus_pemasok.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connect.php';
-session_start();
-
-$id = $_GET['id'];
-$sql = "DELETE FROM pemasok WHERE id_pemasok = '$id'";
-mysqli_query($connection,$sql);
-header('location: pemasok.php');
-?>
